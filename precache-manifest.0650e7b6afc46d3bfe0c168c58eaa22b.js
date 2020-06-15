5b836a0822916e348a4274d60f7e24270e985aac
Shivraj97 <43899907+Shivraj97@users.noreply.github.com>
2020-06-16 01:52:57 +0530

    Updates

---
+++ precache-manifest.0650e7b6afc46d3bfe0c168c58eaa22b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1af7d83d7061f2454ced47e229d542",
+    "url": "/job-listing-react/index.html"
+  },
+  {
+    "revision": "cee0065f60839fffc30a",
+    "url": "/job-listing-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0bf1f9f9e499c9196776",
+    "url": "/job-listing-react/static/js/2.00667e30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-listing-react/static/js/2.00667e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee0065f60839fffc30a",
+    "url": "/job-listing-react/static/js/main.cd92629a.chunk.js"
+  },
+  {
+    "revision": "6e3c271e40a9e52bdfc8",
+    "url": "/job-listing-react/static/js/runtime-main.dc2c6226.js"
+  }
+]);
