d0ffb70a994efd27daef3091c17fb4f7874678a5
Shivraj97 <43899907+Shivraj97@users.noreply.github.com>
2020-06-16 01:31:05 +0530

    added filteration

--- src/App.js
+++ src/App.js
@@ -1,27 +1,98 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import JobBoardComponent from "./components/JobBoardComponent";
 import data from "./assets/data.json";
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
     setJobs(data);
   }, []);
 
-  console.log(jobs);
+  const filterFunc = ({ role, level, tools, languages }) => {
+    if (filters.length === 0) {
+      return true;
+    }
+
+    const tags = [role, level];
+
+    if (tools) {
+      tags.push(...tools);
+    }
+    if (languages) {
+      tags.push(...languages);
+    }
+
+    //return tags.some((tag) => filters.includes(tag));
+    return filters.every((filter) => tags.includes(filter));
+  };
+
+  const handleTagClick = (tag) => {
+    // avoid reading duplicate tags
+    if (filters.includes(tag)) return;
+
+    setFilters([...filters, tag]);
+  };
+
+  const handleFilterClick = (passedFilter) => {
+    setFilters(filters.filter((f) => f !== passedFilter));
+  };
+
+  const clearFilters = () => {
+    setFilters([]);
+  };
+
+  const filteredJobs = jobs.filter(filterFunc);
 
   return (
-    <div className="App">
+    <>
       <header className="bg-teal-500 mb-12">
-        <img src="/images/bg-header-desktop.svg" alt="header-background" />
+        <img
+          className="w-full"
+          src="/images/bg-header-desktop.svg"
+          alt="header-background"
+        />
       </header>
-      {jobs.length === 0 ? (
-        <p>Jobs are fetching...</p>
-      ) : (
-        jobs.map((job) => <JobBoardComponent job={job} key={job.id} />)
-      )}
-    </div>
+      <div className="container m-auto">
+        {filters.length > 0 && (
+          <div
+            className={`flex flex-wrap bg-white shadow-md -my-20 mb-16 my-12 mx-10 p-6 rounded z-10 relative`}
+          >
+            {filters.map((filter) => (
+              <Fragment>
+                <span
+                  onClick={() => handleFilterClick(filter)}
+                  className="text-teal-500 bg-teal-100 cursor-pointer font-bold p-2 rounded"
+                >
+                  {filter}
+                </span>
+                <span className="bg-teal-500 font-bold text-md p-2 mr-4 text-teal-100">
+                  Χ
+                </span>
+              </Fragment>
+            ))}
+            <button
+              onClick={clearFilters}
+              className="font-bold text-gray-700 ml-auto"
+            >
+              Clear
+            </button>
+          </div>
+        )}
+        {jobs.length === 0 ? (
+          <p>Jobs are fetching...</p>
+        ) : (
+          filteredJobs.map((job) => (
+            <JobBoardComponent
+              job={job}
+              key={job.id}
+              handleTagClick={handleTagClick}
+            />
+          ))
+        )}
+      </div>
+    </>
   );
 }
 
